0360fe76c9e3391ed0f3ea496e14a2feaa67cfd8
maria-nita <maria-nita@live.com>
2019-04-22 16:54:24 +0100

    new firebase account

--- golden-nuggets/src/firebaseApp.js
+++ golden-nuggets/src/firebaseApp.js
@@ -2,12 +2,12 @@ import firebase from 'firebase'
 
   // Initialize Firebase
   const config = {
-    apiKey: "AIzaSyCFNi8o2uSFCGOjcN9KefNcJwiGTQPxOEk",
-    authDomain: "goldennuggets-b8a52.firebaseapp.com",
-    databaseURL: "https://goldennuggets-b8a52.firebaseio.com",
-    projectId: "goldennuggets-b8a52",
-    storageBucket: "goldennuggets-b8a52.appspot.com",
-    messagingSenderId: "760925300122"
+    apiKey: "AIzaSyDd8rpojKaiYnvOBBWZ4VZLY2b6SJ_Pw0M",
+    authDomain: "nuggets-d611f.firebaseapp.com",
+    databaseURL: "https://nuggets-d611f.firebaseio.com",
+    projectId: "nuggets-d611f",
+    storageBucket: "nuggets-d611f.appspot.com",
+    messagingSenderId: "698965367955"
   };
 
   export const firebaseApp = firebase.initializeApp(config)
